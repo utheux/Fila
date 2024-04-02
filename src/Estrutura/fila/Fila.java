9b8fb28749549f1c3ee1a8a61af60d30fd3f8fb0
Matheus <utheuz_ribeiro@hotmail.com>
2024-04-02 13:59:26 -0300

    "teste"

--- src/Estrutura/fila/Fila.java
+++ src/Estrutura/fila/Fila.java
@@ -1,8 +1,75 @@
 package Estrutura.fila;
 
 public class Fila {
-        int tamanho = 0;
-        int[] elementos =  new int[tamanho];
-        
+    int tamanho = 0;
+    int[] elementos;
+    int inicio;
+    int fim;
+    int quantidade;
+
+
+
+    public Fila(int tamanhoMaximo){
+        this.tamanho = tamanhoMaximo;
+        this.elementos = new int[tamanhoMaximo];
+        this.inicio = 0;
+        this.fim = -1;
+        this.quantidade = 0;
     }
+
+    public void destruirFila(){
+        this.inicio = 0;
+        this.fim = -1;
+        this.quantidade = 0;
+    }
+
+    public boolean filaCheia(){
+        return this.tamanho == this.quantidade;
+    }
+
+    public boolean filaVazia(){
+        return this.quantidade == 0;
+    }
+
+    public void inserirElementos(int elemento){
+        if (filaCheia()){
+            System.out.println("Erro: A fila está cheia");
+        } else {
+            this.fim = (this.fim +1)%tamanho;
+            this.elementos[this.fim] = elemento;
+            this.quantidade = quantidade+1;
+            System.out.println("ultimo elemento: " + this.elementos[this.fim]);
+        }
+
+
+    }
+    public void removerElemento(){
+        if (filaVazia()){
+            System.out.println("Erro: A fila está vazia");
+        } else {
+            int elementoRemovido = elementos[this.inicio];
+            this.inicio = (this.inicio+1)%this.tamanho;
+            this.quantidade = this.quantidade-1;
+            System.out.println("Elemento removido: " + elementoRemovido);
+        }
+    }
+
+
+    public void localizarElemento(int elemento){
+        int posicao = -1;
+        for (int i = 0; i < tamanho-1; i++) {
+            if (elementos[i] == elemento){
+                posicao = i;
+                System.out.println("Posição do elemento: "+posicao);
+                break;
+            }
+        }
+    }
+
+
+
+
+
+
+
 }
--- src/Estrutura/fila/MainFila.java
+++ src/Estrutura/fila/MainFila.java
@@ -1,2 +1,22 @@
-package Estrutura.fila;public class MainFila {
+package Estrutura.fila;
+
+public class MainFila {
+    public static void main(String[] args) {
+        Fila fila = new Fila(5);
+        fila.inserirElementos(2004);
+        fila.inserirElementos(2003);
+        fila.inserirElementos(2001);
+        fila.removerElemento();
+        fila.inserirElementos(10);
+        fila.inserirElementos(21);
+        fila.inserirElementos(23);
+        fila.inserirElementos(5);
+        fila.destruirFila();
+        fila.inserirElementos(3333);
+        fila.inserirElementos(2334);
+        fila.inserirElementos(33434);
+        fila.inserirElementos(34343);
+        fila.inserirElementos(3);
+        fila.localizarElemento(2334);
+    }
 }
